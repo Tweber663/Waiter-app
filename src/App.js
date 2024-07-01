f30df071de9a2ead241ef160071492eaeb795ea4
Tweber663 <tweber.1995@hotmail.com>
2024-07-01 13:59:59 +0200

    building - 5

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Home from "./components/pages/Home";
 import TableDetails from "./components/views/TableDetails";
@@ -13,14 +13,14 @@ const App = ()  => {
   return (
 
     <Container className={styles.cont}>
-    <Routes>
-      <Route path="/" element={<Welcome/>}/>
-      <Route path="/home" element={<Home/>}/>
-      <Route path="home/tables/:id" element={<TableDetails/>} />
-      <Route path="/orders" element={<Orders/>} />
-      <Route path="/menu" element={<Menu/>}/>
-      <Route path="*" element={<NotFound/>}/>
-     </Routes>
+    <Router basename="/">
+      <Route path="/" component={<Welcome />} />
+      <Route path="/home" component={<Home />} />
+      <Route path="/home/tables/:id" component={<TableDetails />} />
+      <Route path="/orders" component={<Orders />} />
+      <Route path="/menu" component={<Menu />} />
+      <Route path="*" component={<NotFound />} />
+    </Router>
   </Container>
   )
 }
