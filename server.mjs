3e746061d4f02c152020188e07a4427b8542f382
Tweber663 <tweber.1995@hotmail.com>
2024-07-01 17:15:38 +0200

    building - 111

--- server.mjs
+++ server.mjs
@@ -1,12 +1,17 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 import jsonServer from 'json-server';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const server = jsonServer.create();
-const router = jsonServer.router('build/db/app.json');
+const router = jsonServer.router('public/db/app.json');
 const middlewares = jsonServer.defaults({
   static: 'build',
   noCors: true
 });
+
 const port = process.env.PORT || 3130;
 
 server.use(middlewares);
@@ -21,7 +26,7 @@ server.use(router);
 
 // Serve index.html for all non-API routes
 server.get('*', (req, res) => {
-  res.sendFile(path.resolve('build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 server.listen(port, () => {
